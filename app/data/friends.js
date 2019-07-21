39d9bbac7ce00f25449eeeb0babf2135ccc7cd53
Cara Sunberg <cnsunberg@gmail.com>
2019-07-21 17:00:00 -0400

    Fixed issue with CDN incompatibility--both bootstrap and express functions now work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,21 @@
 // Array holding all of the user answers to the survey
 
-var surveyArr = [{hello: "hello"}
+var surveyArr = [
+    {name: "Bob",
+    photo: "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwiAvYnK8sbjAhWqUt8KHWrKDDUQjRx6BAgBEAU&url=https%3A%2F%2Fmodernfarmer.com%2F2017%2F12%2F6-facts-sheep-might-not-know%2F&psig=AOvVaw2xpu_sHBtlbtzxZj0M8Zoo&ust=1563828848236511",
+    options: [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "5",
+    "4",
+    "3",
+    "2",
+    "1"
+    ]
+    }
 
 ];
 
