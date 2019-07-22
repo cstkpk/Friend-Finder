b34ad8a3882a43612b9833e3e4eb01377af9b14f
Cara Sunberg <cnsunberg@gmail.com>
2019-07-22 17:42:02 -0400

    surveyArr options are successfully parsed into integers, but still need to have each object as a separate array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,9 +14,27 @@ module.exports = function(app) {
         surveyData.push(req.body);
         res.json(true);
 
-        
+        var newPerson = req.body.options;
+        var newPersonInt = [];
+        for (var i = 0; i < newPerson.length; i++) {
+            newPersonInt.push(parseInt(newPerson[i]));
+        }
+        console.log(newPersonInt);
+
+        // var allPerson = [];
+        var allPersonInt = [];
+        for (var i = 0; i < surveyData.length - 1; i++) {
+            console.log(surveyData[i].options);
+            // allPerson.push(surveyData[i].options)
+            // console.log(allPerson);
+            for (var j = 0; j < surveyData[i].options.length; j++) {
+                allPersonInt.push(parseInt(surveyData[i].options[j]));
+            }
+        }
+        console.log(allPersonInt);
+        console.log(surveyData.length);
     });
 
 };
 
-console.log(surveyData);
+// console.log(surveyData);
